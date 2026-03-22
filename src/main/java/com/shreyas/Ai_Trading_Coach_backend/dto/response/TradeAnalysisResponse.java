0d7e53692fb25e21f0ed0032af2182b23cf089df
shreyashkashyapanand01 <shreyashkashyapanand01@gmail.com>
2026-03-23 02:52:28 +0530

    update : added lambok gtter & setters

--- src/main/java/com/shreyas/Ai_Trading_Coach_backend/dto/response/TradeAnalysisResponse.java
+++ src/main/java/com/shreyas/Ai_Trading_Coach_backend/dto/response/TradeAnalysisResponse.java
@@ -1,8 +1,13 @@
 package com.shreyas.Ai_Trading_Coach_backend.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
+@Setter
+@Getter
 public class TradeAnalysisResponse {
 
     private String analysisId;
@@ -16,27 +21,4 @@ public class TradeAnalysisResponse {
 
     // Getters & Setters
 
-    public String getAnalysisId() { return analysisId; }
-    public void setAnalysisId(String analysisId) { this.analysisId = analysisId; }
-
-    public String getGeneratedAt() { return generatedAt; }
-    public void setGeneratedAt(String generatedAt) { this.generatedAt = generatedAt; }
-
-    public int getRiskScore() { return riskScore; }
-    public void setRiskScore(int riskScore) { this.riskScore = riskScore; }
-
-    public String getTraderType() { return traderType; }
-    public void setTraderType(String traderType) { this.traderType = traderType; }
-
-    public List<String> getMistakes() { return mistakes; }
-    public void setMistakes(List<String> mistakes) { this.mistakes = mistakes; }
-
-    public Map<String, Object> getMetrics() { return metrics; }
-    public void setMetrics(Map<String, Object> metrics) { this.metrics = metrics; }
-
-    public String getSummary() { return summary; }
-    public void setSummary(String summary) { this.summary = summary; }
-
-    public List<String> getSuggestions() { return suggestions; }
-    public void setSuggestions(List<String> suggestions) { this.suggestions = suggestions; }
 }
